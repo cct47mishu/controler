408034fca8f0602c2053dfe609c8ea689000c6a9
cct47mishu <123601430+cct47mishu@users.noreply.github.com>
2024-09-26 21:17:11 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,2 @@
-#!/bin/bash; sudo apt-get update -y && sudo apt-get install python3-pip -y  && python3 -m pip install -r requirements.txt && nohup python3 app.py > app.log 2>&1 & echo "Flask app started and running in the background. Logs can be found in app.log."
+#!/bin/bash; 
+sudo apt-get update -y && sudo apt install python3-pip -y  && python3 -m pip install -r requirments.txt && nohup python3 app.py > app.log 2>&1 &
